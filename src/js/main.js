afacf6657354aca17d2fd83b0529ae3c341ba4e8
Sylvain Lesage <severo@rednegra.net>
2019-06-04 11:11:32 +0200

    refactor: use @gridify/oddata instead of @gridify/kernel

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,13 @@
-import {
-  createDataset,
-  getMetadataFile,
-  getMetadataFilesList,
-} from '@gridify/kernel/src/index.js';
-import {fetchRelativeJson} from './fetch.js';
+import {get} from '@gridify/oddata';
 
 async function fillSelect() {
-  const list = await getMetadataFilesList(fetchRelativeJson, 'dataset.json');
+  const list = await get();
   const select = document.querySelector('select#datasets-list');
-  list.forEach(async metadataFile => {
-    try {
-      const rawMetadata = await getMetadataFile(
-        fetchRelativeJson,
-        metadataFile
-      );
-      const metadata = createDataset(rawMetadata);
-      const option = document.createElement('option');
-      option.value = metadata.file;
-      option.text = metadata.name;
-      select.appendChild(option);
-    } catch (error) {
-      if (error instanceof SyntaxError) {
-        // eslint-disable-next-line no-console
-        console.warn(metadataFile + ' is a badly formatted JSON - ignored.');
-      } else {
-        throw error;
-      }
-    }
+  list.forEach(async dataset => {
+    const option = document.createElement('option');
+    option.value = dataset.raw.Item;
+    option.text = dataset.title;
+    select.appendChild(option);
   });
 }
 
