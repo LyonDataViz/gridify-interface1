b4548035afd7bbe062a5eec0ead6dc12d95801f7
Sylvain Lesage <severo@rednegra.net>
2019-06-04 12:40:00 +0200

    fix: function is no more async

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ import {get} from '@gridify/oddata';
 async function fillSelect() {
   const list = await get();
   const select = document.querySelector('select#datasets-list');
-  list.forEach(async dataset => {
+  list.forEach(dataset => {
     const option = document.createElement('option');
     option.value = dataset.raw.Item;
     option.text = dataset.title;
