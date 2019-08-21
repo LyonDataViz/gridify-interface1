5286a40517149490c37e06318dbac6b3726586e9
Sylvain Lesage <severo@rednegra.net>
2019-08-21 12:00:39 +0200

    refactor: use @gridify/oddata instead of @gridify/kernel

--- src/js/fetch.js
+++
@@ -1,5 +0,0 @@
-const endpoint =
-  'https://raw.githubusercontent.com/LyonDataViz/oddata-public/master/';
-
-export const fetchRelativeJson = (relativeFile, apiEndpoint = endpoint) =>
-  fetch(apiEndpoint + relativeFile).then(d => d.json());
