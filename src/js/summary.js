3f641f65a97be0f9e9b349b669767aaa68f5d29e
Sylvain Lesage <severo@rednegra.net>
2019-08-21 12:00:40 +0200

    feat: show 100 first rows in a handsontable element

--- src/js/summary.js
+++ src/js/summary.js
@@ -1,8 +1,51 @@
+const MAX_ROWS = 100;
+async function fillTable(tableEl, dataset) {
+  const previousHandsontableEl = tableEl.querySelector('.handsontable');
+  if (previousHandsontableEl) {
+    previousHandsontableEl.remove();
+  }
+  const handsontableEl = document.createElement('div');
+  tableEl.append(handsontableEl);
+
+  const data = await dataset.data.then(data => data.slice(0, MAX_ROWS));
+
+  var hotSettings = {
+    data: data,
+    columns: dataset.raw.attributes.map(att => {
+      // Add dates
+      return {
+        data: att.name,
+        type: att.type === 'quantitative' ? 'numeric' : 'text',
+      };
+    }),
+    stretchH: 'all',
+    width: 880,
+    autoWrapRow: true,
+    height: 487,
+    autoColumnSize: {
+      samplingRatio: 23,
+    },
+    filters: true,
+    dropdownMenu: true,
+    rowHeaders: true,
+    colHeaders: dataset.raw.attributes.map(att => att.name),
+    columnSorting: {
+      indicator: true,
+    },
+    licenseKey: 'non-commercial-and-evaluation',
+  };
+
+  // eslint-disable-next-line no-undef
+  new Handsontable(handsontableEl, hotSettings);
+}
+
 export function prepare(dispatch) {
   const summaryEl = document.querySelector('#summary');
   const titleEl = summaryEl.querySelector('#summary-title');
+  const tableEl = summaryEl.querySelector('#summary-table');
 
   dispatch.on('select-dataset', dataset => {
-    titleEl.textContent = dataset.title;
+    titleEl.textContent = `${dataset.title} - ${MAX_ROWS} first rows`;
+    fillTable(tableEl, dataset);
   });
 }
